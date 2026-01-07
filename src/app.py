22dfbe94072550d46d040c251f046541c98a2462
yessigarridowork-blip <yessigarrido.work@gmail.com>
2026-01-07 01:20:51 +0000

    mi app starwars

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Film, Planet, Vehicle
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,100 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def user():
+    all_user = User.query.all()
+    all_user = list(map(lambda u: u.serialize(), all_user))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(all_user), 200
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['GET'])
+def get_people():
+
+    all_people = People.query.all()
+    all_people = list(map(lambda p: p.serialize(), all_people))
+
+    return jsonify(all_people), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id(people_id):
+
+    people = People.query.get(people_id)
+    if people is None:
+        return jsonify({"message": "People not found"}), 404
+
+    return jsonify(people.serialize()), 200 
+
+@app.route('/film', methods=['GET'])
+def get_film():
+
+    all_film = Film.query.all()
+    all_film = list(map(lambda f: f.serialize(), all_film))
+
+    return jsonify(all_film), 200
+
+@app.route('/film/<int:film_id>', methods=['GET'])
+def get_film_id(film_id):
+
+    film = Film.query.get(film_id)
+    if film is None:
+        return jsonify({"message": "Film not found"}), 404
+
+    return jsonify(film.serialize()), 200 
+
+
+@app.route('/planet', methods=['GET'])
+def get_planet():
+
+    all_planet = Planet.query.all()
+    all_planet = list(map(lambda f: f.serialize(), all_planet))
+
+    return jsonify(all_planet), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet_id(planet_id):
+
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        return jsonify({"message": "Planet not found"}), 404
+
+    return jsonify(planet.serialize()), 200 
+
+
+@app.route('/vehicle', methods=['GET'])
+def get_vehicle():
+
+    all_vehicle = Vehicle.query.all()
+    all_vehicle = list(map(lambda f: f.serialize(), all_vehicle))
+
+    return jsonify(all_vehicle), 200
+
+@app.route('/vehicle/<int:vehicle_id>', methods=['GET'])
+def get_vehicle_id(vehicle_id):
+
+    vehicle = Vehicle.query.get(vehicle_id)
+    if vehicle is None:
+        return jsonify({"message": "Vehicle not found"}), 404
+
+    return jsonify(vehicle.serialize()), 200 
+
+@pp.
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
